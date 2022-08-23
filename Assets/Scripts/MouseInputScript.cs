316a19bdee5fc1e1211fcc6064929cc7f45efe5d
Lucas de Jager <lucasdejager@gmail.com>
2022-08-23 21:16:28 +0200

    scripts :)

---
+++ Assets/Scripts/MouseInputScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInputScript : MonoBehaviour
+{
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            GameObject obj = GetObjectUnderMouse();
+            obj.GetComponent<Clickable>().Click();
+        }
+    }
+
+    GameObject GetObjectUnderMouse() {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+        if (hit.collider != null) {
+            return hit.collider.gameObject;
+        }
+
+        return null;
+    }
+}
---
+++ Assets/Scripts/StateManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable {
+    public string name;
+    public GameObject obj;
+    public string[] states;
+}
+
+public class StateManager : MonoBehaviour 
+{
+    public bool 
+}
